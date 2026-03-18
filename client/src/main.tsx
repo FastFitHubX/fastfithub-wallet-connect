6c0cd2ceaf66122f0daa7c5c0368ca802aac6026
FastFitHub <ceo@fastfithub.com>
2026-03-18 22:03:24 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,26 +1,9 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { WagmiProvider } from 'wagmi'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { createWeb3Modal } from '@web3modal/wagmi/react'
-import { config, projectId } from './config/wallet'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-// 1. Setup queryClient
-const queryClient = new QueryClient()
-
-// 2. Create modal
-createWeb3Modal({
-  wagmiConfig: config,
-  projectId,
-  enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  enableOnramp: true // Optional - false as default
-})
-
-createRoot(document.getElementById("root")!).render(
-  <WagmiProvider config={config}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </WagmiProvider>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
